4c1a8235da95eb80d05a9c784faef4a4bfcc7377
AhsanRiaz9 <bcsf18m009@pucit.edu.pk>
2021-11-15 01:03:51 +0500

    Remove Bugs in Practice Question

--- app/src/main/java/com/example/mc_task3_bcsf18m009/MainActivity3.java
+++ app/src/main/java/com/example/mc_task3_bcsf18m009/MainActivity3.java
@@ -1,7 +1,10 @@
 package com.example.mc_task3_bcsf18m009;
 
+import static android.graphics.Color.parseColor;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -9,24 +12,38 @@ import android.widget.TextView;
 
 import java.util.Random;
 
-public class MainActivity3 extends AppCompatActivity {
+public class MainActivity3 extends AppCompatActivity implements View.OnClickListener {
 
     Button nextBtn;
-    TextView alphabet;
+    TextView alphabet, correctAns;
     QuestionPool questionPool;
     Question q;
     Button op1,op2,op3,op4;
-
+    int defaultColor;
+    int correctColor;
+    int wrongColor;
+    boolean flag = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
+        // color initlization
+        defaultColor = parseColor("#78432C");
+        correctColor = parseColor("#0f8425");
+        wrongColor = parseColor("#b71249");
         nextBtn = findViewById(R.id.nextBtn);
         alphabet = findViewById(R.id.arabicLetter);
+        correctAns = findViewById(R.id.correctAns);
         op1 = findViewById(R.id.op1);
         op2 = findViewById(R.id.op2);
         op3 = findViewById(R.id.op3);
         op4 = findViewById(R.id.op4);
+        nextBtn.setOnClickListener(this);
+        op1.setOnClickListener(this);
+        op2.setOnClickListener(this);
+        op3.setOnClickListener(this);
+        op4.setOnClickListener(this);
+
         questionPool = new QuestionPool();
         q = questionPool.getRandomQuestion();
         alphabet.setText(q.letter);
@@ -34,19 +51,6 @@ public class MainActivity3 extends AppCompatActivity {
         op2.setText(q.op2);
         op3.setText(q.op3);
         op4.setText(q.op4);
-
-        nextBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v)
-            {
-                q = questionPool.getRandomQuestion();
-                alphabet.setText(q.letter);
-                op1.setText(q.op1);
-                op2.setText(q.op2);
-                op3.setText(q.op3);
-                op4.setText(q.op4);
-            }
-        });
     }
 
     public String getRandomOption()
@@ -63,6 +67,80 @@ public class MainActivity3 extends AppCompatActivity {
         Random rand = new Random();
         int index = rand.nextInt(28);
         return alphabet[index];
+    }
+
+    @Override
+    public void onClick(View v)
+    {
+        switch (v.getId())
+        {
+            case R.id.nextBtn:
+                q = questionPool.getRandomQuestion();
+                alphabet.setText(q.letter);
+                op1.setText(q.op1);
+                op2.setText(q.op2);
+                op3.setText(q.op3);
+                op4.setText(q.op4);
+                flag = false;
+                resetColor();
+                correctAns.setText("");
+                break;
+            case R.id.op1:
+                if(flag==false)
+                {
+                    changeBtnColors(op1,isCorrectAnswer(op1.getText().toString()));
+                    correctAns.setText("Correct: " + q.correctOption.toUpperCase());
+                    flag = true;
+                }
+                break;
+            case R.id.op2:
+                if(flag==false)
+                {
+                    changeBtnColors(op2,isCorrectAnswer(op2.getText().toString()));
+                    correctAns.setText("Correct: " + q.correctOption.toUpperCase());
+                    flag = true;
+                }
+                break;
+            case R.id.op3:
+                if(flag==false)
+                {
+                    changeBtnColors(op3,isCorrectAnswer(op3.getText().toString()));
+                    correctAns.setText("Correct: " + q.correctOption.toUpperCase());
+                    flag = true;
+                }
+                break;
+            case R.id.op4:
+                if(flag==false)
+                {
+                    changeBtnColors(op4,isCorrectAnswer(op4.getText().toString()));
+                    correctAns.setText("Correct: " + q.correctOption.toUpperCase());
+                    flag = true;
+                }
+                break;
+        }
+    }
+    void changeBtnColors(Button btn, boolean dec)
+    {
+        if(dec==true)
+        {
+            btn.setBackgroundColor(correctColor);
+        }
+        else
+        {
+            btn.setBackgroundColor(wrongColor);
+        }
+    }
+    void resetColor()
+    {
+        op1.setBackgroundColor(defaultColor);
+        op2.setBackgroundColor(defaultColor);
+        op3.setBackgroundColor(defaultColor);
+        op4.setBackgroundColor(defaultColor);
+    }
+
+    boolean isCorrectAnswer(String ans)
+    {
+        return ans==q.correctOption;
     }
 
 }
