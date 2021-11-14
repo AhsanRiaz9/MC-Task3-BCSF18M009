e6247dd6f629691cea3efd2826ec9cb42d319e81
AhsanRiaz9 <bcsf18m009@pucit.edu.pk>
2021-11-15 00:08:14 +0500

    Added QuestionPool and Question Class to generate random question

--- app/src/main/java/com/example/mc_task3_bcsf18m009/MainActivity3.java
+++ app/src/main/java/com/example/mc_task3_bcsf18m009/MainActivity3.java
@@ -13,37 +13,55 @@ public class MainActivity3 extends AppCompatActivity {
 
     Button nextBtn;
     TextView alphabet;
+    QuestionPool questionPool;
+    Question q;
+    Button op1,op2,op3,op4;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
         nextBtn = findViewById(R.id.nextBtn);
         alphabet = findViewById(R.id.arabicLetter);
+        op1 = findViewById(R.id.op1);
+        op2 = findViewById(R.id.op2);
+        op3 = findViewById(R.id.op3);
+        op4 = findViewById(R.id.op4);
+        questionPool = new QuestionPool();
+        q = questionPool.getRandomQuestion();
+        alphabet.setText(q.letter);
+        op1.setText(q.op1);
+        op2.setText(q.op2);
+        op3.setText(q.op3);
+        op4.setText(q.op4);
+
         nextBtn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                alphabet.setText(getRandomOption());
+            public void onClick(View v)
+            {
+                q = questionPool.getRandomQuestion();
+                alphabet.setText(q.letter);
+                op1.setText(q.op1);
+                op2.setText(q.op2);
+                op3.setText(q.op3);
+                op4.setText(q.op4);
             }
         });
-    }
-    publin int getRandomInt()
-    {
-        Random rand = new Random();
-        return rand.nextInt(30);
     }
 
     public String getRandomOption()
     {
-        String [] catLists = {"Makharij","Lahatiyah","Shajariyah-Haafiyah","Tarfiyah","Nit-eeyah","Lisaveyah","Ghunna"};
-        String [] alphabet = {"۱","ب","پ","ت","ث","ج","ح","خ","د", "ذ","ر","ز",
-                "س","ش","ص",
-                "ض","ط","ظ",
-                "ع","غ","ف",
-                "ق","ک","ل",
-                "م","ن","ہ",
-                "و","ی"};
+        String [] catLists = {"Halqiyah","Lahatiyah","Shajariyah-Haafiyah","Tarfiyah","Nit-eeyah","Lisaveyah","Ghunna"};
+        String [] alphabet = {"۱","ب","ت","ث","ج","ح","خ","د","ذ","ر","ز","س","ش","ص","ض","ط","ظ","ع","غ","ف","ق","ک","ل","م","ن","ہ","و","ی"};
+        String [] s1 = {"ا", "ہ", "ع","ح","غ","خ"};
+        String [] s2 = {"ق","ک"};
+        String [] s3 = {"ج","ش","ی","ض"};
+        String [] s4 = {"ل","ن","ر"};
+        String [] s5 = {"ط","د","ت"};
+        String [] s6 = {"ظ","ذ","ث"};
+        String [] s7 = {"م","ن"};
         Random rand = new Random();
-        int index = rand.nextInt(30);
+        int index = rand.nextInt(28);
         return alphabet[index];
     }
 
